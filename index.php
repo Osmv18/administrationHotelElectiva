6f609c2864434762131fa7fd5bbf8948621ee983
Einzgartig96 <teresita.guido51@gmail.com>
2021-08-09 01:46:33 -0600

    update

--- index.php
+++ index.php
@@ -1,12 +1,7 @@
 <!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
 <html>
-  <head>
-    <meta charset="UTF-8">
+    <head>
+        <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Hoteles</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
@@ -14,17 +9,22 @@ and open the template in the editor.
         <script src="jquery.min.js"></script>
         <!-- CSS only -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link rel="stylesheet" href="css/style.css">
+        <!-- FONTS -->
+        <link href="http://fonts.cdnfonts.com/css/alyanda" rel="stylesheet">
+        <link href="http://fonts.cdnfonts.com/css/parmapetit" rel="stylesheet">                
         <!-- JavaScript Bundle with Popper -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
         <!-- JQuery -->
         <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-  </head>
-  <body>
-    <h6>
-     <!--PHP to control connection traffic based on MVC-->
+        
+    </head>
+    <body>
+        <!--PHP to control connection traffic based on MVC-->
         <?php
 
         require 'view/header.php';
@@ -35,7 +35,6 @@ and open the template in the editor.
             include 'view/inicio.php';
         }        
         require 'view/footer.php';
-        ?>  
-     </h6>
-  </body>
+        ?>    
+    </body>
 </html>
--- view/inicio.php
+++ view/inicio.php
@@ -1,3 +1,88 @@
 <div class="main-container">
-    Administración de hoteles
+    <div class="contenedor-carrusel">
+        <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+            <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+                <div class="carousel-item active" id="slide">
+                    <img src="img/hotel_inicio.jpg" class="d-block w-100" alt="hotelImg">
+                    <div class="carousel-caption d-none d-md-block" > 
+                        <h5>#YoAmoVacacionar</h5>
+                        <p>Puedes reservar una habitación para ti o para toda tu familia y disfrutar al máximo.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" id="slide">
+                    <img src="img/habitacion_inicio.jpg" class="d-block w-100" alt="hotelImg">
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Los mejores hoteles</h5>
+                        <p>Contamos con Resorts todo incluido a lo largo del país.</p>
+                    </div>
+                </div>
+                <div class="carousel-item" id="slide">
+                    <img src="img/privado_inicio.jpg" class="d-block w-100" alt="hotelImg" >
+                    <div class="carousel-caption d-none d-md-block">
+                        <h5>Privacidad asegurada </h5>
+                        <p>Tenemos disponibilidad de suites con alberca privada para usted y su familia.</p>
+                    </div>
+                </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden"></span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden"></span>
+            </button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row" id="bienvenida">
+            <h2 id="Titulo-centro">¡ Vacaciones en familia !</h2>
+            <div id="parrafo-centro" class="col-xs-12 col-sm-12 col-md-12">
+            <p>OBLIVION Hotel Resort; un lugar sensacional y extraordinario, 
+            con 5 sedes ubicadas a lo largo y ancho de todo el país. Cada uno de nuestros hoteles 
+            cuenta con una vista única, playas muy limpias y espacios amplios. 
+            En OBLIVION Hotel Resort estamos preparados para dar el mejor servicio a nuestros clientes. </p>
+            </div>
+        </div>
+        <div class="row" id="divisor-row">
+            <img src="img/divider.png" alt="divider" id="divisor">
+        </div>
+        <div class="row" id="bienvenida-2">              
+            <div id="centro" class="col-xs-12 col-sm-7 col-md-7">
+                <h2 id="Titulo-centro-2"> ¿Qué ofrecemos?</h2>
+                <p id="parrafo-centro">
+                ✾	Piscina de agua dulce y terraza solárium. <br>
+                ✾	Jacuzzi, gimnasio, baños de vapores, spa y centro médico de emergencia. <br>
+                ✾	Variedad de sabores en restaurante tipo buffet o diferentes restaurantes temáticos. 
+                (Esto varía con respecto a la sede donde decida alojarse). <br>
+                ✾	Salas de conferencia (Disponible sólo en la Sede de Puntarenas). <br>
+                ✾	Entretenimiento y actividades para todas las edades; gimnasia, tenis y tenis de mesa, 
+                billar, voleibol, acu-aeróbics, golf y teatro con marionetas para los más pequeños. <br>
+                ✾	Guardería hasta medio día. </p>
+            </div>
+            <div id="derecha" class="col-xs-12 col-sm-4 col-md-4">
+                <img src="img/vacaciones.jpg" alt="vacaciones_familiares" id="vacaciones">
+            </div>  
+        </div>    
+        <div class="row" id="divisor-row">
+            <img src="img/divider.png" alt="divider" id="divisor">
+        </div>
+        <div class="row" id="bienvenida" >
+            <h2 id="Titulo-centro-3">Lo más destacado de nuestros hoteles.</h2>
+            <p id="parrafo-centro-3">  Todo Incluido las 24 horas. ✾ <br>
+             WiFi gratuito en todo el hotel. ✾<br>
+             Amplio programa de entretenimiento para niños y adultos con el equipo de animadores de Oblivion. ✾<br>
+             3 piscinas y jacuzzi.  ✾<br>
+             Snacks las 24 horas del día. ✾<br>
+             Suites familiares con piscina privada. ✾<br>
+             Restaurante principal y restaurantes temáticos. ✾<br>
+             Parque acuático para todas las edades. (Los niños deben ser acompañados por un adulto) ✾</p>
+        </div>    
+    </div>
 </div>
+
