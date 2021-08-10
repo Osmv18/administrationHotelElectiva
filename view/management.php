60678476c4feb8147d3a4db5eab96c61bc32e114
Einzgartig96 <teresita.guido51@gmail.com>
2021-08-09 19:25:35 -0600

    update user, admin, css & index files

--- view/management.php
+++ view/management.php
@@ -1,3 +1,95 @@
-<div class="container">
-    <h6>Administración</h6>
+<div class="main-container">
+    <div id="admin-inicial">
+    <h2 id="admin-title">Administración de habitaciones.</h2>
+    </div>
+    <p id="user-descrp">Seleccione la habitación para observar detalle, editar o eliminar la información.</p>
+    <div class="tz-gallery">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/suit1.jpeg" alt="suit_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Suit 1 </p>
+                        <p id="descrip-habitacion">No disponible. Ver detalles.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/suit2.jpg" alt="suit_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Suit 2 </p>
+                        <p id="descrip-habitacion">No disponible. Ver detalles.</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/suit3.jpg" alt="suit_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Suit 3 </p>
+                        <p id="descrip-habitacion">No disponible. Ver detalles.</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion1.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 1 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion2.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 2 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion3.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 3 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion4.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 4 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion5.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 5 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion6.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 6 </p>
+                        <p id="descrip-habitacion">Disponible. Editar detalles</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>    
 </div>
--- view/user.php
+++ view/user.php
@@ -1,3 +1,69 @@
-<div class="container">
-    <h6>Nuevos huespedes</h6>
+<div class="main-container">
+    <div id="user-inicial">
+    <h2 id="user-title">Nuestras habitaciones...</h2>
+    </div>
+    <p id="user-descrp">En oblivion te ofrecemos las mejores habitaciones al precio justo.</p>
+    <div class="tz-gallery">
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion1.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 1 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡500,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion2.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 2 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡600,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion3.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 3 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡700,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-xs-12 col-sm-4 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion4.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 4 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡250,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion5.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 5 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡150,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-4">
+                <a class="lightbox" href="#">
+                    <img src="img/album_habitacion6.jpg" alt="habitacion_img" id="habitacion_img">
+                    <div class="carousel-caption d-none d-md-block">
+                        <p id="titulo-habitacion"> Habitación 6 </p>
+                        <p id="descrip-habitacion">Disponible desde ₡300,000. Ver más...</p>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>    
 </div>
+
+
+
